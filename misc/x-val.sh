70d06b91ce0f3f0d36a97d5722ff000d4b0aaccc
Joel <rootjdev@gmail.com>
2022-11-08 09:17:20 +1030

    pipe the StdDev stuff to >/dev/null

--- misc/x-val.sh
+++ misc/x-val.sh
@@ -4,4 +4,4 @@ source ../_internal/_vars
 
 # this is probbaly not needed
 # test ! -d "./node_modules/" && PATH="${PATH}:${NODE_PATH}" npm install
-PATH="${PATH}:${NODE_PATH}" ./x-val.js "${@}"
+PATH="${PATH}:${NODE_PATH}" ./x-val.js "${@}" 2>/dev/null
